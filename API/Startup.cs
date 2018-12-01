f3b5d0703f25e77dd76e5ea2ba69ddbd625488dc
Neil Cummings <neilc@outlook.com>
2018-12-01 10:24:34 +0700

    Modified Values controller to retrieve data from DB

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Persistence;
 
 namespace API
 {
@@ -24,6 +26,10 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<DataContext>(c => 
+      {
+        c.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+      });
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
